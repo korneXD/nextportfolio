c92f9cffba4bdf6873d70a7b8cdadf154edd6a94
korneXD <korcika2005@gmail.com>
2025-02-28 10:32:50 +0100

    mobile nav

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -1,11 +1,16 @@
+import { useState } from "react"
+
 export default function Header() {
+    const [open, setOpen] = useState(false)
     return (
         <header className="flex justify-center items-center w-full h-fit absolute top-0 left-0 z-20">
             <nav className="h-fit w-full flex justify-center items-center py-6 px-10">
                 <div className="flex flex-1 h-fit justify-start items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-gray-200">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
-                    </svg>
+                    <button onClick={() => setOpen(!open)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-gray-200">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+                        </svg>
+                    </button>
                 </div>
                 <div className="flex flex-1 h-fit justify-center items-center">
                     <span className="text-gray-200 italic tracking-widest">WWW</span>
@@ -16,6 +21,9 @@ export default function Header() {
                     </button>
                 </div>
             </nav>
+            {open &&
+                <h1 className="text-white">Haló</h1>
+            }
         </header>
     )
 }
