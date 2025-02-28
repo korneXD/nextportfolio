431e51dae26bcb8257fc8569d443323848a3e20c
korneXD <korcika2005@gmail.com>
2025-02-28 13:28:25 +0100

    next

---
+++ src/app/works/page.js
@@ -0,0 +1,8 @@
+
+export default function Works() {
+    return (
+        <div className="flex min-h-screen w-full bg-black">
+            <h1 className="text-white">Works</h1>
+        </div>
+    )
+}
