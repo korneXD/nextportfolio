84784b48e86b026c72b2bc5144dc33cc549856f0
korneXD <korcika2005@gmail.com>
2025-03-04 00:08:19 +0100

    update

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -58,7 +58,8 @@ export default function ContactPage() {
             </div>
             <div data-scroll data-scroll-speed="0.05" className="flex z-10 justify-center px-10 py-10 items-center md:items-start w-full md:w-fit h-fit absolute bottom-10 left-0 flex-col">
                 <span className="text-gray-200 uppercase text-md md:text-lg tracking-wider text-left w-full px-8 italic">Halmosi Kornél</span>
-                <h1 className="text-white tracking-tighter text-5xl  md:text-7xl" style={{ fontFamily: "Arial Black" }}>
+                <h1 className="text-white tracking-tighter text-5xl md:text-7xl font-extrabold" style={{ fontFamily: "Arial Black, Arial, sans-serif" }}
+                >
                     <span className="inline-block transform skew-x-6">CON</span>
                     <span className="inline-block transform -skew-x-8">TACT</span>
                 </h1>
