f65eba89e6a4c4b0d65d1f6e5350be09ec1c017a
korneXD <korcika2005@gmail.com>
2025-03-03 19:58:43 +0100

    locomotive scroll

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import Header from "@/components/menu/header";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
-import { useRef } from "react";
 
 export const metadata = {
   title: "Kornél",
@@ -10,29 +9,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const [locomotive, setLocomotive] = useState(null);
-  const container = useRef(null)
-
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const scroll = new LocomotiveScroll({
-        el: container,
-        smooth: true,
-        smartphone: { smooth: true },
-        tablet: { smooth: true },
-      });
-      setLocomotive(scroll);
-    })();
-
-    return () => {
-      if (locomotive) locomotive.destroy();
-    };
-  }, [locomotive]);
   return (
     <html lang="en">
       <body
-        ref={container}
         data-scroll-container
         className={`antialiased bg-black overflow-x-hidden scroll-smooth`}
       >
