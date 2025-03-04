af7f2600f78912a6133d2038f9cd84d41f17ddc8
korneXD <korcika2005@gmail.com>
2025-03-04 23:33:54 +0100

    next

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,6 +1,6 @@
 export const menuLinks = [
     { name: "Főoldal", path: "/" },
-    { name: "Munkáim", path: "/works" },
+    { name: "Projekteim", path: "/works" },
     { name: "Szolgáltatásaim", path: "/services" },
     { name: "Elérhetőség", path: "/contact" },
 ];
